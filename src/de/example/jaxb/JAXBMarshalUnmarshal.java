be3477898eb7dc552e906ef06b4cf96edb457da9
Johannes Salentin <Johannes.Salentin@gmail.com>
2019-06-07 11:15:15 +0200

    marshalling functionality
    renamed package for future use

---
+++ src/de/example/jaxb/JAXBMarshalUnmarshal.java
@@ -0,0 +1,66 @@
+package de.example.jaxb;
+
+import de.example.model.ModelType;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import java.io.File;
+
+public class JAXBMarshalUnmarshal {
+
+    // mainly used for testing
+    public static void main(String[] args) throws JAXBException, SAXException {
+        ModelType model = unmarshal("CentralModel.xml", ModelType.class, null);
+        System.out.println(model.getElements().getElement().get(0).getIdentifier());
+        //marshal(model, "Test.xml", null);
+    }
+
+    /**
+     * Unmarshalling of provided XML-File
+     *
+     * @param xmlFile   The XML-File to be unmarshalled
+     * @param c         The corresponding root class
+     * @param xsdSchema XSD-Schema for optional validation
+     * @param <T>       The type of the root class
+     * @return The corresponding object to the unmarshalled XML-File
+     * @throws JAXBException for unmarshalling error
+     * @throws SAXException  for schema error
+     */
+    public static <T> T unmarshal(String xmlFile, Class<T> c, String xsdSchema) throws JAXBException, SAXException {
+        JAXBContext jc = JAXBContext.newInstance(c.getPackage().getName());
+        Unmarshaller unmarshaller = jc.createUnmarshaller();
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = (xsdSchema == null || xsdSchema.trim().length() == 0)
+                ? null : schemaFactory.newSchema(new File(xsdSchema));
+        unmarshaller.setSchema(schema);
+        return unmarshaller.unmarshal(new StreamSource(new File(xmlFile)), c).getValue();
+    }
+
+    /**
+     * Marshalling of provided Object to specified XML-File
+     *
+     * @param jaxbElement The object to be marshalled
+     * @param xmlFile     The output XML-File
+     * @param xsdSchema   XSD-Schema for optional validation
+     * @throws JAXBException for marshalling error
+     * @throws SAXException  for schema error
+     */
+    public static void marshal(Object jaxbElement, String xmlFile, String xsdSchema) throws JAXBException, SAXException {
+        JAXBContext jc = JAXBContext.newInstance(jaxbElement.getClass().getPackage().getName());
+        Marshaller marshaller = jc.createMarshaller();
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = (xsdSchema == null || xsdSchema.trim().length() == 0)
+                ? null : schemaFactory.newSchema(new File(xsdSchema));
+        marshaller.setSchema(schema);
+        marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        marshaller.marshal(jaxbElement, new File(xmlFile));
+    }
+}
