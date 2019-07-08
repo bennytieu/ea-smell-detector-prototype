46e4935031aaa0de4f907c40781f592470b6c838
Johannes Salentin <Johannes.Salentin@gmail.com>
2019-07-08 11:47:06 +0200

    configuration of test plugin

--- src/test/java/de/example/smells/TestCyclicDependency.java
+++ src/test/java/de/example/smells/TestCyclicDependency.java
@@ -1,18 +1,24 @@
 package de.example.smells;
 
 import de.example.main.ModelAdapter;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestCyclicDependency {
 
-    private ModelAdapter model = new ModelAdapter("Test.xml", null);
-    private CyclicDependency detector = new CyclicDependency();
+    private CyclicDependency detector;
+
+    @BeforeAll
+    void init() {
+        ModelAdapter model = new ModelAdapter("Test.xml", null);
+        Detector.setModel(model);
+        detector = new CyclicDependency();
+    }
 
     @Test
     void test() {
-        Detector.setModel(model);
         assertEquals(detector.detect().size(), 3);
     }
 }
