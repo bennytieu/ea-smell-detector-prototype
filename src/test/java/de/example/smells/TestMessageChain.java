535127378262e4863b7f5e031d90a29b12ec81ec
Johannes Salentin <Johannes.Salentin@gmail.com>
2019-07-12 15:37:02 +0200

    new tests

--- src/test/java/de/example/smells/TestMessageChain.java
+++ src/test/java/de/example/smells/TestMessageChain.java
@@ -1,7 +1,6 @@
 package de.example.smells;
 
 import de.example.main.ModelAdapter;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,15 +9,19 @@ class TestMessageChain {
 
     private MessageChain detector;
 
-    @BeforeAll
-    void init() {
-        ModelAdapter model = new ModelAdapter("Test.xml", null);
+    @Test
+    void testCentralModel() {
+        ModelAdapter model = new ModelAdapter("CentralModel.xml", null);
         Detector.setModel(model);
         detector = new MessageChain();
+        assertEquals(detector.detect().size(), 0);
     }
 
     @Test
-    void test() {
+    void testSmellExample() {
+        ModelAdapter model = new ModelAdapter("SmellExample.xml", null);
+        Detector.setModel(model);
+        detector = new MessageChain();
         assertEquals(detector.detect().size(), 1);
     }
 }
